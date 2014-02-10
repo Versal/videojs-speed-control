a8c1cf180f6f196f8f8df922e4a232bc8160b9ec
Ryan <ryan.joshua.jones@gmail.com>
2014-02-09 21:05:17 -0800

    select works

--- src/videojs.speed-control.js
+++ src/videojs.speed-control.js
@@ -16,17 +16,74 @@
    */
   vjs.SpeedControl = vjs.MenuItem.extend({
     init: function(player, options) {
-      var speedSelect = document.createElement('select');
-
       vjs.MenuItem.call(this, player, options);
-      this.player().el().appendChild(speedSelect);
     }
   });
 
-  function speedControl(options) {
-    var opts = options || {};
+  function createSpeedControl() {
+    var props = {
+      className: 'vjs-speed-control vjs-control',
+      innerHTML: buildSpeeds(),
+      role: 'button',
+      'aria-live': 'polite',
+      tabIndex: 0
+    };
 
-    return new vjs.SpeedControl(this, opts);
+    return vjs.Component.prototype.createEl(null, props);
+  }
+
+  /**
+   * @returns {String} innerHTML string of <option> elements for select
+   */
+  function buildSpeeds() {
+    // speeds are hard-coded for now - could pass in speeds instead
+    // from plugin setup, if necessary
+    var speeds = [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75],
+        resultHTML = '<select class="vjs-speed-select">';
+
+    for (var speed in speeds) {
+      // make 1.0x the default speed
+      if (speeds[speed] === 1) {
+        resultHTML += '<option value="' + speeds[speed] + '"' +
+          'selected="selected">' +
+          speeds[speed] + '.0x</option>';
+      } else {
+        resultHTML += '<option value="' + speeds[speed] + '"' +
+          '>' + speeds[speed] + 'x</option>';
+      }
+    }
+
+    return resultHTML += '</select>';
+  }
+
+  /**
+   * hook up events to the speed selector
+   * @param {Object} select DOM obj of select element
+   * @param {Object} player Reference to videojs player
+   */
+  function setSpeedEvents(select, player) {
+    // setup an onChange listener on the speed select
+    // and bind it to the player
+    select.onchange = function(e) {
+      var index = e.target.selectedIndex,
+          newRate = e.target.options[index].value;
+
+      // player.M === direct way to access <video>
+      this.M.playbackRate = newRate;
+    }.bind(player);
+  }
+
+  function speedControl() {
+    var options = { 'el': createSpeedControl() },
+        speed = new vjs.SpeedControl(this, options),
+        select;
+
+    // append the speed select to the videojs control bar
+    this.controlBar.el().appendChild(speed.el());
+    select = document.getElementsByClassName('vjs-speed-select')[0];
+
+    // pass in select and reference to player to events function
+    return setSpeedEvents(select, this);
   }
 
   vjs.plugin('speedControl', speedControl);
